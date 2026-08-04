e535f7b03ba8ebc9111898024f6c54fd0a4e4b47
SamDeCqa <samwelsikanyika88@gmail.com>
2026-08-04 10:38:16 +0300

    Added Role Migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_verified')->default(false);
             $table->string('profile_photo');
             $table->string('cover_photo');
             $table->uuid()->unique();
