805469c3ab135e6c60f92a08c96d8b56bfd133db
SamDeCqa <samwelsikanyika88@gmail.com>
2026-08-06 11:24:52 +0300

    Fixed Media dummy data generator

--- database/factories/MediaFactory.php
+++ database/factories/MediaFactory.php
@@ -27,6 +27,7 @@ class MediaFactory extends Factory
             'description' => fake()->boolean(70) ? fake()->realText() : null,
             'is_favorite' => fake()->boolean(30),
             'user_id' => User::inRandomOrder()->value('id'),
+            'is_private' => fake()->boolean(20),
             'category_id' => fake()->boolean(73) ? Category::inRandomOrder()->value('id') : null,
             'byte_size' => rand(589, 14896),
             ];
