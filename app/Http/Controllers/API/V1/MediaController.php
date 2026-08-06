aa6c2b451fc6f04130788dcdbcca66cca368dc74
SamDeCqa <samwelsikanyika88@gmail.com>
2026-08-06 11:10:34 +0300

    Querying all the medias

--- app/Http/Controllers/API/V1/MediaController.php
+++ app/Http/Controllers/API/V1/MediaController.php
@@ -7,15 +7,94 @@ use App\Http\Requests\StoreMediaRequest;
 use App\Http\Requests\UpdateMediaRequest;
 use App\Http\Resources\MediaResource;
 use App\Models\Media;
+use Illuminate\Http\Request;
 
 class MediaController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $media = Media::with(['category'])->get();
+        $query = Media::query()->with(['category', 'tags']) //???????????: JE NAWEZA PATA MEDIA ALIZOWEKA ADMIN ZIWE AVAILABLE PUBLICLY KWA KILA MEMBER
+        ->where(
+            fn($q) => $q->where(
+                    fn($inner) => $inner->where(
+                        'user_id',
+                        '=',
+                        null
+                        )
+                        ->orWhereRelation(
+                        'category',
+                        'user_id',
+                        '=',
+                        null
+                    )
+                        ->orWhereRelation(
+                            'tags',
+                            'user_id',
+                            '=',
+                            null
+                        )
+                )
+        )
+        ->orWhereNot(
+                fn($q) => $q->where(
+                    fn($inner) => $inner->where(
+                        'user_id',
+                        '!=',
+                        $request->user()->id 
+                        )
+                        ->orWhereRelation(
+                        'category',
+                        'user_id',
+                        '!=',
+                        $request->user()->id //HAKUNA KUCHUKUA MEDIA ZA CATEGORY AMBAYO USER HUSIKA HAJAITENGENEZA YEYE
+                    )
+                        ->orWhereRelation(
+                            'tags',
+                            'user_id',
+                            '!=',
+                            $request->user()->id //HAKUNA KUCHUKUA MEDIA ZENYE TAGS AMBAZO USER HUSIKA HAKUZITENGENEZA YEYE
+                        )
+                )
+            );
+
+        $query->when(
+            $request->has('by_me'), //KUPATA MEDIA ZOTE ALIZOHIFADHI USER
+            fn($q) => $q->where('user_id', $request->user()->id)
+            /* fn($q) => $q->where(
+                fn($inner) => $inner->where('user_id', $request->user()->id)//MEDIA ZENYE HIYO USER ID
+                    ->orWhereRelation(
+                        'category',
+                        'user_id',
+                        '=',
+                        $request->user()->id//MEDIA AMBAYO SIO YA HUYO USER ILA YEYE AMEI-CATEGORISE KIVYAKE
+                    )
+            )*/
+        );
+
+        $query->when(
+            $request->filled('category'),
+            fn($inner) => $inner->whereRelation(
+                'category',
+                'name',
+                'ilike',
+                '%' . $request->category . '%' //MEDIA ILIYOPO CATEGORY FLANI
+            )
+        );
+
+        $query->when(
+            $request->filled('tags'),
+            fn($inner) => $inner->whereRelation(
+                'tags',
+                'name',
+                'ilike',
+                '%' . $request->tags . '%' //MEDIA ILIYOPEWA TAG FLANI
+            )
+        );
+
+        $media = $query->cursorPaginate(70);
 
         return MediaResource::collection($media);
     }
--- app/Http/Resources/MediaResource.php
+++ app/Http/Resources/MediaResource.php
@@ -20,11 +20,13 @@ class MediaResource extends JsonResource
             'url' => $this->path,
             'type' => $this->content_type,
             'info' => $this->description,
+            'user_id(For Dbg)' => $this->user_id,
             'user' => $this->whenLoaded('user', fn() => new UserResource($this->user)),
             'category' => $this->whenLoaded('category', fn() => new CategoryResource($this->category)),
+            'tags' => $this->whenLoaded('tags', fn() => TagResource::collection($this->tags)),
             'size_in_bytes' => $this->byte_size,
             'metadata' => $this->metadata,
-            'is_favorite' => $this->is_favourite,
+            'is_favorite' => $this->is_favorite,
             'uploaded_on' => $this->created_at
         ];
     }
